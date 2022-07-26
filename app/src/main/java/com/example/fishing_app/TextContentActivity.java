84de1be7859e393772a9733b53532fb929a5fb69
Valera <fedosv.valera95@gmail.com>
2022-07-26 14:15:32 +0300

    fifth

--- app/src/main/java/com/example/fishing_app/TextContentActivity.java
+++ app/src/main/java/com/example/fishing_app/TextContentActivity.java
@@ -1,6 +1,7 @@
 package com.example.fishing_app;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.ScrollView;
@@ -13,9 +14,9 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 public class TextContentActivity extends AppCompatActivity {
     private int category;
     private int position;
+    private Typeface typeface;
     private TextView text_content_view;
     private ImageView image_сontent;
-    private ConstraintLayout cnst;
     private int[] fishes_array = {R.string.fish_1,R.string.fish_2,R.string.fish_3,R.string.fish_4,R.string.fish_5};
     private int[] image_fishes_array = {R.drawable.distr,R.drawable.jp1,R.drawable.jp2,R.drawable.jp3,R.drawable.jp4};
 
@@ -23,10 +24,15 @@ public class TextContentActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.text_content);
+        init();
+        receiveIntent();
+    }
+
+    private void init() {
         text_content_view = findViewById(R.id.text_content_view);
         image_сontent = findViewById(R.id.image_сontent);
-        cnst = findViewById(R.id.cnst);
-        receiveIntent();
+        typeface = Typeface.createFromAsset(this.getAssets(), "fonts/Merriweather-Italic.ttf");
+        text_content_view.setTypeface(typeface);
     }
 
     private void receiveIntent() {
